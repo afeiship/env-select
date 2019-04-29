8819d47fa25e8ef6bd1ab1d3ec56c0e571d1c4d1
afeiship <1290657123@qq.com>
2019-04-29 19:17:34 +0800

    add: hello allo

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import nx from 'next-js-core2';
 
 class App {
   static start() {
-    console.log('hello!', nx.VERSION);
+    console.log('hello babel-node!', nx.VERSION);
   }
 }
 
+// main start:
 App.start();
