8e16b74159e3ef6bf0291e5ddf49a8fcb1d8a283
afeiship <1290657123@qq.com>
2020-06-28 20:41:37 +0800

    feat: add api folder

--- src/setup.js
+++ src/setup.js
@@ -2,7 +2,14 @@ import '@feizheng/next-js-core2';
 import '@feizheng/next-require';
 
 // config
-nx.require({ scope: ['dependencies'] });
+nx.require({
+  scope: ['dependencies'],
+  pattern: [
+    '@feizheng/next-*',
+    '@afeiship/next-*',
+    '!@feizheng/next-require'
+  ]
+});
 nx.global({
   conf: new nx.YamlConfiguration({ path: './config.yml' })
 });
