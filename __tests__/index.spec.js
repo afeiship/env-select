6f369e09cb798c37108ad27d7ac735ce6b320389
afeiship <1290657123@qq.com>
2022-06-08 22:23:24 +0800

    test: add spec file

---
+++ __tests__/index.spec.js
@@ -0,0 +1,40 @@
+const { CraEnvs, AbstractEnvSelect } = require('../dist/index.cjs');
+
+describe('test cra-envs', () => {
+  test('cra-envs: get all', function () {
+    const res = CraEnvs.get();
+    expect(res).toEqual({
+      beta: {
+        base_url: 'https://site-predict-platform.beta.saybot.net',
+      },
+      staging: {
+        base_url: 'https://site-predict-platform.staging.saybot.net',
+      },
+    });
+  });
+});
+
+describe('test abstract-env-select:', () => {
+  var glb = { url: 'null' };
+
+  class MyEnvSelect extends AbstractEnvSelect {
+    static select() {
+      if (glb.url.includes('beta')) {
+        return 'beta';
+      }
+      return 'staging';
+    }
+  }
+
+  test('abstract-env-select: get beta from env', function () {
+    glb.url = 'xx.beta.url';
+    const res = MyEnvSelect.get('base_url');
+    expect(res).toBe('https://site-predict-platform.beta.saybot.net');
+  });
+
+  test('abstract-env-select: get staging from env', function () {
+    glb.url = 'xx.yyy.url';
+    const res = MyEnvSelect.get('base_url');
+    expect(res).toBe('https://site-predict-platform.staging.saybot.net');
+  });
+});
