48af224534ea6b3584aa8a41d6cadd7e85bf7e01
afeiship <1290657123@qq.com>
2022-06-09 15:25:20 +0800

    fix: cra-envs

--- src/cra-envs.ts
+++ src/cra-envs.ts
@@ -1,11 +1,12 @@
 import nx from '@jswork/next';
+import CraAppEnvs from '@jswork/cra-envs';
 
 const ENVS_KEY = '__envs__';
 
 export default class CraEnvs {
   static version = '__VERSION__';
   static get() {
-    const penv = process.env;
+    const penv = CraAppEnvs.get('envs');
     const tenv = {};
     nx.forIn(penv, (key, value) => {
       if (key.includes(ENVS_KEY)) {
@@ -20,6 +21,6 @@ export default class CraEnvs {
     nx.forIn(env, (key, value) => {
       res[ENVS_KEY + key] = JSON.stringify(value);
     });
-    return { envs: res };
+    return CraAppEnvs.set({ envs: res });
   }
 }
