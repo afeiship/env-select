998f70148d70f80f7d69c5f26df92b8830a44f38
afeiship <1290657123@qq.com>
2023-11-26 10:45:42 +0800

    fix: camelize to result

--- src/_utils.ts
+++ src/_utils.ts
@@ -3,11 +3,14 @@
 // './src/stores/modules/sub/test-sbc.ts'         ---> sub/test-sbc
 // ./modules/sub/test.ts                          ---> sub/test
 
+// replace sub/test/child ---> subTestChild
+const camelize = (inStr: string) => inStr.replace(/\/(\w)/g, (_, c) => c.toUpperCase());
+
 export const replaceModule = (inputPath: string) => {
   const end = inputPath.split('.').pop();
   const start = inputPath.includes('modules') ? 'modules/' : './';
   const regex = new RegExp(`${start}(.*?).${end}`);
   const match = inputPath.match(regex);
   const matched = match && match[1];
-  return matched ? match[1] : inputPath;
+  return matched ? camelize(match[1]) : inputPath;
 };
