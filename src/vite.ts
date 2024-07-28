588c39ff7ce056850ddf9d21a453092646e503a1
afeiship <1290657123@qq.com>
2024-07-28 21:38:03 +0800

    feat: add transform opt

--- src/vite.ts
+++ src/vite.ts
@@ -7,12 +7,17 @@ interface Options {
   modules?: string;
   keyFn?: (key: string) => string;
   valueFn?: (value: any, context: any) => any;
+  transform?: (key: string, value, context: any) => any;
 }
 
 const defaults: Options = {
   modules: 'modules/',
   valueFn: (_: any, context: any) => context?.default || context,
   keyFn: (key: string) => path2mod(key),
+  transform: (key, value, context) => {
+    context[key] = value;
+    return context;
+  },
 };
 
 const scan = (inModuleFiles: ModuleFiles, inOptions?: Options): Record<string, any> => {
@@ -22,9 +27,9 @@ const scan = (inModuleFiles: ModuleFiles, inOptions?: Options): Record<string, a
   const keys = Object.keys(inModuleFiles);
   return keys.reduce((acc: any, key: string) => {
     const value = inModuleFiles[key];
-    const _key = toPath(key);
-    acc[options.keyFn!(_key)] = options.valueFn!(key, value);
-    return acc;
+    const _key = options.keyFn!(toPath(key));
+    const _value = options.valueFn!(key, value);
+    return options.transform?.(_key, _value, acc);
   }, {});
 };
 
