4f9add320ad5c1e3adcf67a158ba9a19751e7efa
afeiship <1290657123@qq.com>
2024-07-28 11:23:07 +0800

    fix: keyFn update

--- src/vite.ts
+++ src/vite.ts
@@ -11,18 +11,18 @@ interface Options {
 const defaults: Options = {
   modules: 'modules/',
   valueFn: (_: any, context: any) => context?.default || context,
+  keyFn: (key: string) => key,
 };
 
 const scan = (inModuleFiles: ModuleFiles, inOptions?: Options): Record<string, any> => {
   const options = { ...defaults, ...inOptions };
-  options.keyFn = options.keyFn || function(key: string) {
-    return replaceModule(key, options.modules);
-  };
+  const toPath = (key: string) => replaceModule(key, options.modules);
 
   const keys = Object.keys(inModuleFiles);
   return keys.reduce((acc: any, key: string) => {
     const value = inModuleFiles[key];
-    acc[options.keyFn!(key)] = options.valueFn!(key, value);
+    const _key = toPath(key);
+    acc[options.keyFn!(_key)] = options.valueFn!(key, value);
     return acc;
   }, {});
 };
